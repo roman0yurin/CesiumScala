4a53e618a2ca566e011bec97b483325298f5609d
roman0yurin <gooood85@yandex.ru>
2021-02-09 01:46:24 +0500

    Настройка освещения

---
+++ src/main/scala/c60/sjs/cesium/scene/SunLight.scala
@@ -0,0 +1,14 @@
+package c60.sjs.cesium.scene
+
+import scala.scalajs.js
+import scala.scalajs.js.annotation.JSGlobal
+
+/**
+* Имитация естественного направления солнечного света с учетом времени суток.
+* @author Юрин Роман 08.02.21
+**/
+@js.native
+@JSGlobal("Cesium.SunLight")
+class SunLight() extends Light{
+
+}
